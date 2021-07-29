84f431d5db9aa7820a0a265c012a23f38c270ee1
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-07-29 14:10:00 -0400

    feat: add cjs option

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-tsc --outDir dist
+tsc --outDir dist --incremental false --tsBuildInfoFile null
+tsc --outDir dist/cjs --module commonjs --incremental false --tsBuildInfoFile null
 cp LICENSE package.json README.md dist
+node -e "console.log(JSON.stringify({...$(cat package.json), type: \"commonjs\"}, null, 2))" > dist/cjs/package.json
