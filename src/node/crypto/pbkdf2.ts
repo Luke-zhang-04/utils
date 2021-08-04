04aaa07b1651692c477103253308681c05e958c4
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-08-03 23:53:56 -0400

    feat(node/crypto): add deriveKeySync

--- src/node/crypto/pbkdf2.ts
+++ src/node/crypto/pbkdf2.ts
@@ -48,4 +48,29 @@ export const deriveKey = async (
         )
     })
 
+/**
+ * Provides an synchronous Password-Based Key Derivation Function 2 (PBKDF2) implementation.
+ *
+ * @remarks
+ * Synchronous operations block the main thread and may cause performance issues in larger applications
+ * @param secretKey - Secret key for encryption. The key length is dependent on the algorithm of
+ *   choice. The key length in bytes (characters) is equal to the key length in bits divided by the
+ *   number of bits in a byte (8)
+ *
+ *   - AES-128 - 16 bytes
+ *   - AES-192 - 24 bytes
+ *   - AES-256 - 32 bytes
+ *
+ * @param salt - Random salt for key. The salt should be as unique as possible. It is recommended
+ *   that a salt is random and at least 16 bytes long.
+ * @param algorithm - Digest algorithm
+ * @returns Derived secret key
+ */
+export const deriveKeySync = (
+    secretKey: string,
+    salt: Buffer,
+    // istanbul ignore next
+    algorithm: HashAlgorithms = "sha256",
+): Buffer => crypto.pbkdf2Sync(secretKey, salt, iterations, secretKey.length, algorithm)
+
 export default deriveKey
