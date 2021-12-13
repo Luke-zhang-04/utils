6f19fe3ec5bc4100e54b8ad4423aa10fddff2151
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2021-12-13 15:50:54 +0000

    test(fetch): improve coverage

--- __tests__/src/browser/fetch.ts
+++ __tests__/src/browser/fetch.ts
@@ -27,6 +27,25 @@ describe("fetch", () => {
             expect(Object.keys(body).length).toBeGreaterThan(0)
         })
 
+        it("should not throw error if request returns in time", async () => {
+            const response = await fetchWithTimeout(
+                "https://api.spacexdata.com/v4/launches/latest",
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    timeout: 10_000,
+                },
+            )
+
+            expect(response.status).toBe(200)
+
+            const body = await response.json()
+
+            expect(Object.keys(body).length).toBeGreaterThan(0)
+        })
+
         it("should abort fetch", async () => {
             const response = await inlineTryPromise(() =>
                 fetchWithTimeout("https://fakeresponse.com/api/?sleep=100", {
--- src/browser/fetch.ts
+++ src/browser/fetch.ts
@@ -6,16 +6,25 @@
  * @author Luke Zhang (https://luke-zhang-04.github.io)
  */
 
+type FetchOptions = Exclude<RequestInit, "signal"> & {
+    /**
+     * Timeout in ms
+     */
+    timeout?: number
+}
+
 /**
  * Fetch API with timeout
  *
  * @param resource - Resource to fetch
  * @param param1 - Fetch parameters + an optional timeout to abort the fetch after time
+ * @param timeout - Timeout in ms
  * @returns Response
  */
 export const fetchWithTimeout = async (
     resource: RequestInfo,
-    {timeout, ...options}: Exclude<RequestInit, "signal"> & {timeout?: number} = {},
+    // istanbul ignore next
+    {timeout, ...options}: FetchOptions = {},
 ): Promise<Response> => {
     if (timeout === undefined) {
         return await fetch(resource, options)
