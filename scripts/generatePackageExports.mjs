ea4108c2e16950f653d5856a81ec9f07806797f3
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2022-02-20 12:04:56 -0500

    scripts: generate exports using wildcards

--- scripts/generatePackageExports.mjs
+++ scripts/generatePackageExports.mjs
@@ -6,37 +6,43 @@ import util from "util"
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-const src = (await util.promisify(glob)(`${__dirname}/../dist/src/**/*.ts`)).map((file) =>
-    file.replace(/^.*\/src\//u, "").replace(/\.ts$/u, ""),
-)
+// No
+const src = Array.from(
+    new Set(
+        (await util.promisify(glob)(`${__dirname}/../dist/src/**/*.ts`)).map((file) =>
+            file.replace(/^.*\/src\//u, "").replace(/(^|\/)[a-zA-Z1-9.\-_]+$/u, ""),
+        ),
+    ),
+).sort((a, b) => {
+    if (!b) {
+        return -1
+    }
 
-const exports = {}
+    return b.slice("/").length - a.slice("/").length
+})
+
+const exports = {
+    ".": {
+        require: "./cjs/index.js",
+        import: "./index.js",
+    },
+}
 
 for (const file of src) {
-    if (file === "index") {
-        exports["."] = {
-            require: "./cjs/index.js",
-            import: "./index.js",
-        }
-
-        exports["./index"] = {
-            require: "./cjs/index.js",
-            import: "./index.js",
-        }
-    } else if (file.endsWith("index")) {
-        exports[`./${file.replace(/\/index$/u, "")}`] = {
-            require: `./cjs/${file}.js`,
-            import: `./${file}.js`,
-        }
-
-        exports[`./${file}`] = {
-            require: `./cjs/${file}.js`,
-            import: `./${file}.js`,
+    if (!file) {
+        exports["./*"] = {
+            require: "./cjs/*.js",
+            import: "./*.js",
         }
     } else {
+        exports[`./${file}/*`] = {
+            require: `./cjs/${file}/*.js`,
+            import: `./${file}/*.js`,
+        }
+
         exports[`./${file}`] = {
-            require: `./cjs/${file}.js`,
-            import: `./${file}.js`,
+            require: `./cjs/${file}/index.js`,
+            import: `./${file}/index.js`,
         }
     }
 }
