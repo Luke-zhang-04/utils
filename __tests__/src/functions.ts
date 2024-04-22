a1b82375f15d95acdb6dc520518c58dcf13a0ce5
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2024-04-21 23:01:56 -0400

    feat: add debounce

--- __tests__/src/functions.ts
+++ __tests__/src/functions.ts
@@ -4,7 +4,7 @@
  * 0BSD License
  */
 
-import {runIfDefined} from "../../src/functions"
+import {debounce, runIfDefined} from "../../src/functions"
 
 describe("functions", () => {
     describe("runIfDefined", () => {
@@ -20,6 +20,59 @@ describe("functions", () => {
             const value = runIfDefined("string", testFunction)
 
             expect(value).toBe("+string")
+        })
+    })
+
+    describe("debounce", () => {
+        const wait = (amt: number) => new Promise((resolve) => setTimeout(resolve, amt))
+
+        let amt = 0
+        const testFunction = (inc: number) => (amt += inc)
+
+        test("should call function immediately with 0 timeout", async () => {
+            amt = 0
+
+            debounce(() => testFunction(1), 0)()
+            debounce(() => testFunction(2), 0)()
+            debounce(() => testFunction(3), 0)()
+
+            await wait(0)
+            expect(amt).toEqual(6)
+        })
+
+        test("should cancel", async () => {
+            amt = 0
+
+            const one = debounce(() => testFunction(1), 10)
+            const two = debounce(() => testFunction(2), 20)
+            const three = debounce(() => testFunction(3), 30)
+
+            one()
+            one()
+            one()
+            two()
+            two()
+            three()
+
+            await wait(30)
+            expect(amt).toEqual(6)
+
+            amt = 0
+
+            one()
+            two()
+            three()
+
+            await wait(20)
+            expect(amt).toEqual(3)
+
+            one()
+            two()
+            three()
+
+            await wait(30)
+
+            expect(amt).toEqual(9)
         })
     })
 })
--- src/functions.ts
+++ src/functions.ts
@@ -29,3 +29,35 @@
  */
 export const runIfDefined = <T, R>(value: T | undefined, func: (param: T) => R): R | undefined =>
     value === undefined ? undefined : func(value)
+
+/**
+ * Delay the execution of a function, and reset said delay if the function is called again within
+ * the delay window.
+ *
+ * @example
+ *
+ * ```ts
+ * window.addEventListener(
+ *     "resize",
+ *     debounce((event) => console.log(event), 100),
+ * )
+ * ```
+ *
+ * @param func - Function to debounce
+ * @param ms - Time to wait
+ * @returns Debounced function
+ */
+export const debounce = <Args extends unknown[]>(
+    func: (...args: Args) => void,
+    ms: number,
+): ((...args: Args) => void) => {
+    let timeout: ReturnType<typeof setTimeout> | undefined = undefined
+
+    return (...args: Args) => {
+        clearTimeout(timeout)
+
+        timeout = setTimeout(() => {
+            func(...args)
+        }, ms)
+    }
+}
